e66911490c9ebf9844c53948ae5d913cdac01d61
Nurchos <nurmuhammednasidrinov61@gamil.com>
2026-05-20 17:33:06 +0300

    подправил функцию удаления

--- main.py
+++ main.py
@@ -1,22 +1,71 @@
+import flet as ft
+
 from db.database import init_db
 from db.queries import (
-    add_student,
     get_students,
-    update_student,
+    add_student,
     delete_student
 )
 
 
+def main(page: ft.Page):
+    page.title = "Students App"
+
+    column = ft.Column()
+
+    name = ft.TextField(label="Name")
+    age = ft.TextField(label="Age")
+    course = ft.TextField(label="Course")
+
+    def load_students(e=None):
+        column.controls.clear()
+
+        students = get_students()
+
+        for s in students:
+            sid = s[0]
+
+            column.controls.append(
+                ft.Row([
+                    ft.Text(f"{s[1]} | {s[2]} | {s[3]}"),
+                    ft.IconButton(
+                        icon=ft.Icons.DELETE,
+                        on_click=lambda e, sid=sid: remove_student(sid)
+                    )
+                ])
+            )
+
+        page.update()
+
+    def add_new(e):
+        add_student(
+            name.value,
+            int(age.value),
+            course.value
+        )
+
+        load_students()
+
+    def remove_student(student_id):
+        delete_student(student_id)
+        load_students()
+
+    page.add(
+        name,
+        age,
+        course,
+        ft.ElevatedButton(
+            "Add",
+            on_click=add_new
+        ),
+        ft.ElevatedButton(
+            "Load students",
+            on_click=load_students
+        ),
+        column
+    )
+
+
 init_db()
 
-add_student("Alex", 18, "Python")
-add_student("John", 20, "Backend")
-
-students = get_students()
-
-for student in students:
-    print(student)
-
-update_student(1, "Mike")
-
-delete_student(2)
+ft.app(target=main)
